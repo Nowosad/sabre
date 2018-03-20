495fffb1ff438107a821628c874fab736d4d64a0
Nowosad <tupiszakaczki@gmail.com>
2018-03-20 16:22:48 -0400

    region prep cleaned vol1

--- R/regions_prep.R
+++ R/regions_prep.R
@@ -3,7 +3,7 @@
 #' The output of this function can be used in [sabre_calc()] and [mapcurves_calc()].
 #'
 #' @param y DESC
-#' @param x An output of the [spatial_prep()] function
+#' @param x DESC
 #' @param x_name DESC
 #' @param y_name DESC
 #'
@@ -11,13 +11,16 @@
 #'
 #' @details DETAILS
 #'
+#' @importFrom sf st_intersection st_area
+#' @importFrom stats aggregate
+#'
 #' @examples
 #' # EXAMPLES
 #'
 #' @export
 regions_prep = function(x, y, x_name, y_name){
-        v1 = vector_select_class(x, x_name) %>% st_make_valid()
-        v2 = vector_select_class(y, y_name) %>% st_make_valid()
+        v1 = vector_select_class(x, x_name) #%>% st_make_valid()
+        v2 = vector_select_class(y, y_name) #%>% st_make_valid()
 
         suppressWarnings({inter_v12 = st_intersection(v1, v2)})
         inter_v12$area = as.numeric(st_area(inter_v12))
@@ -29,9 +32,15 @@ regions_prep = function(x, y, x_name, y_name){
 }
 
 vector_select_class = function(vector_obj, attr_name){
-        vector_obj = vector_obj[attr_name]
-        vector_obj = aggregate(vector_obj, by = list(vector_obj[[attr_name]]), function(x) x[1], do_union = FALSE)
-        # vector_obj = na.omit(vector_obj)
-        vector_obj = st_cast(vector_obj, "MULTIPOLYGON")
-        vector_obj
+  vector_obj = vector_obj[attr_name]
+  unique_classes = unique(vector_obj[[attr_name]])
+  if (nrow(vector_obj) == length(unique_classes)){
+    return(vector_obj)
+  } else {
+    vector_obj = aggregate(vector_obj, by = list(g = vector_obj[[attr_name]]),
+                           FUN = function(x) x[1], do_union = FALSE)
+    # vector_obj = st_cast(vector_obj, "MULTIPOLYGON")
+    vector_obj = vector_obj[attr_name]
+    return(vector_obj)
+  }
 }
