b24879e84ce7cb44b2f79dafc10c1bc87781b35f
Nowosad <tupiszakaczki@gmail.com>
2018-03-20 13:50:52 -0400

    mapcurves function added

---
+++ R/mapcurves.R
@@ -0,0 +1,65 @@
+#' Mapcurves
+#'
+#' Mapcurves: a quantitative method for comparing categorical maps
+#'
+#' @param x A numeric vector of counts
+#' @param y A numeric vector of counts
+#' @param z A numeric matrix of counts
+#'
+#' @return A list
+#'
+#' @details DETAILS
+#'
+#' @references Hargrove, William W., Forrest M. Hoffman, and Paul F. Hessburg.
+#' "Mapcurves: a quantitative method for comparing categorical maps."
+#' Journal of Geographical Systems 8.2 (2006): 187.
+#'
+#' @examples
+#' # EXAMPLES
+#'
+#' @export
+mapcurves = function(x, y, z = NULL){
+        if (is.null(z)){
+                # crosstable of categories
+                xy_table = table(x, y)
+                z = xy_table^2 / tcrossprod(rowSums(xy_table), colSums(xy_table))
+        }
+
+        # row and col sums
+        z_xsum = c(0, sort(rowSums(z)), 1)
+        z_ysum = c(0, sort(colSums(z)), 1)
+
+        # values shares
+        share_x = c(1, seq(1, 0, length.out = nrow(z)), 0)
+        share_y = c(1, seq(1, 0, length.out = ncol(z)), 0)
+
+        # data.frame plot
+        df_x = data.frame(x = z_xsum, y = share_x, type = "x")
+        df_y = data.frame(x = z_ysum, y = share_y, type = "y")
+
+        # # plot
+        # df = rbind(df_x, df_y)
+        # library(ggplot2)
+        # print(ggplot(df, aes(x, y, color = type)) + geom_point() + geom_line())
+
+        # area under curve
+        gof_x = area_under_curve(df_x$x, df_x$y)
+        gof_y = area_under_curve(df_y$x, df_y$y)
+
+        # output
+        if (gof_x >= gof_y){
+                gof = gof_x
+                map = "x"
+        } else {
+                gof = gof_y
+                map = "x"
+        }
+
+        # result
+        result = list(map = map, gof = gof)
+        return(result)
+}
+
+area_under_curve = function(x, y){
+        sum(diff(x) * (head(y, -1) + tail(y, -1))) / 2
+}
