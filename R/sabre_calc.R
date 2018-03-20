e835b9f341523683be32c4e42cadfabb7d40ffcc
Nowosad <tupiszakaczki@gmail.com>
2018-03-20 16:23:18 -0400

    sabre_calc fix and clean

--- R/sabre_calc.R
+++ R/sabre_calc.R
@@ -1,9 +1,9 @@
 #' SABRE calculation
 #'
 #' It calculate the Spatial Association Between REgionalizations (SABRE) using
-#' the output of the [spatial_prep()] function.
+#' the output of the [regions_prep()] function.
 #'
-#' @param x An output of the [spatial_prep()] function
+#' @param x An output of the [regions_prep()] function
 #' @param unit A logarithm base ("log", "log2" or "log10")
 #' @param B DESC
 #'
@@ -16,6 +16,8 @@
 #' of the 2007 joint conference on empirical methods in natural language
 #' processing and computational natural language learning (EMNLP-CoNLL). 2007.
 #'
+#' @importFrom entropy entropy.empirical
+#'
 #' @examples
 #' # EXAMPLES
 #'
@@ -24,16 +26,15 @@ sabre_calc = function(x, unit = "log2", B = 1){
         x[[1]]$area = as.numeric(st_area(x[[1]]))
         x[[2]]$area = as.numeric(st_area(x[[2]]))
 
-        x_name = names(x[[1]])[2]
-        y_name = names(x[[2]])[2]
+        x_name = names(x[[1]])[1]
+        y_name = names(x[[2]])[1]
 
-        xy_values_freq = st_set_geometry(x[[3]], NULL)[c("area", x_name, y_name)] %>%
-                tidyr::spread(x_name, "area", fill = 0) %>%
-                .[-1] %>%
-                as.matrix()
+        xy_values_freq = area_spread(x[[3]], name1 = x_name, name2 = y_name)
 
-        x[[1]]$entropy = apply(xy_values_freq, 2, entropy, unit = unit) / entropy(colSums(xy_values_freq), unit = unit) # next divide by entropy of a row var
-        x[[2]]$entropy = apply(xy_values_freq, 1, entropy, unit = unit) / entropy(rowSums(xy_values_freq), unit = unit) # next divide by entropy of a col var
+        x[[1]]$entropy = apply(xy_values_freq, 2, entropy.empirical, unit = unit) /
+          entropy.empirical(colSums(xy_values_freq), unit = unit) # next divide by entropy of a row var
+        x[[2]]$entropy = apply(xy_values_freq, 1, entropy.empirical, unit = unit) /
+          entropy.empirical(rowSums(xy_values_freq), unit = unit) # next divide by entropy of a col var
 
         v_result = v_measure(x[[1]]$area, x[[2]]$area, xy_values_freq, unit = unit, B = B)
         sabre_result = list(x[[1]], x[[2]], v_result)
