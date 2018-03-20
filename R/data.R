32b2d5e5bd1e1d0c7b031ccbbe05fcc985ea4861
Nowosad <tupiszakaczki@gmail.com>
2018-03-20 16:23:31 -0400

    new vector dataset added

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Ecoregions of the United States
+#'
+#' Bailey's Ecoregions of the Conterminous United States
+#'
+#' @source https://www.sciencebase.gov/catalog/item/54244abde4b037b608f9e23d
+"eco_us"
