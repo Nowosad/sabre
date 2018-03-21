13ea0a07090e536b320e25658c35c8088e2d95f5
jn <tupiszakaczki@gmail.com>
2018-03-20 20:36:54 -0400

    conditional v-measure tests

--- R/v_measure.R
+++ R/v_measure.R
@@ -17,29 +17,29 @@
 #' of the 2007 joint conference on empirical methods in natural language
 #' processing and computational natural language learning (EMNLP-CoNLL). 2007.
 #'
-#' @importFrom entropy entropy.empirical mi.empirical
+#' @importFrom infotheo entropy condentropy
 #'
 #' @examples
 #' # EXAMPLES
 #'
 #' @export
-v_measure = function(x, y, z = NULL, unit = "log2", B = 1){
-        entropy_x = entropy.empirical(x, unit = unit)
-        entropy_y = entropy.empirical(y, unit = unit)
+v_measure = function(x, y, z = NULL, B = 1){
+        entropy_x = entropy(x)
+        entropy_y = entropy(y)
         if (is.null(z)){
-                mi_xy = mi.empirical(table(x, y), unit = unit)
+                ce_xy = condentropy(x, y)
         } else {
-                mi_xy = mi.empirical(z, unit = unit)
+                ce_xy = condentropy(z)
         }
         if(entropy_x == 0){
                 homogeneity = 1
         } else {
-                homogeneity = mi_xy / entropy_x
+                homogeneity = 1 - (ce_xy / entropy_x)
         }
         if(entropy_y == 0){
                 completeness = 1
         } else {
-                completeness = mi_xy / entropy_y
+                completeness = 1 - (ce_xy / entropy_y)
         }
         if(homogeneity + completeness == 0){
                 v = 0
