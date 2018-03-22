da63d58eebb1a639092dabb487c24514bed3e1f4
Nowosad <tupiszakaczki@gmail.com>
2018-03-22 16:19:29 -0400

    sabre_calc rlang and mess

--- R/sabre_calc.R
+++ R/sabre_calc.R
@@ -1,7 +1,6 @@
 #' SABRE calculation
 #'
-#' It calculate the Spatial Association Between REgionalizations (SABRE) using
-#' the output of the [regions_prep()] function.
+#' It calculate the Spatial Association Between REgionalizations (SABRE).
 #'
 #' @param y DESC
 #' @param x_name DESC
@@ -20,7 +19,8 @@
 #' processing and computational natural language learning (EMNLP-CoNLL). 2007.
 #'
 #' @importFrom entropy entropy.empirical
-#' @importFrom sf st_intersection st_area
+#' @importFrom sf st_intersection
+#' @importFrom rlang enquo
 #'
 #' @examples
 #' # EXAMPLES
@@ -28,20 +28,35 @@
 #' @export
 sabre_calc = function(x, x_name, y, y_name, unit = "log2", B = 1){
 
+  x = st_set_precision(x, 1)
+  y = st_set_precision(y, 1)
+
   suppressWarnings({z = st_intersection(x, y)})
+  z = st_cast(z, "MULTIPOLYGON")
+
   x = vector_regions(z, !!enquo(x_name))
   y = vector_regions(z, !!enquo(y_name))
 
   z_df = intersection_prep(z, !!enquo(x_name), !!enquo(y_name))
-  x_df = regions_prep(x, !!enquo(x_name))
-  y_df = regions_prep(y, !!enquo(y_name))
 
-  # x_df$entropy = apply(z_df, 2, entropy.empirical, unit = unit) /
-  #   entropy.empirical(colSums(z_df), unit = unit) # next divide by entropy of a row var
-  # y_df$entropy = apply(z_df, 1, entropy.empirical, unit = unit) /
-  #   entropy.empirical(rowSums(z_df), unit = unit) # next divide by entropy of a col var
+  SjZ = apply(z_df, 2, entropy.empirical, unit = "log2")
+  SjR = apply(z_df, 1, entropy.empirical, unit = "log2")
 
-  v_result = v_measure(x = x_df$area, y = y_df$area, z = z_df, unit = unit, B = B)
+  SR = entropy.empirical(colSums(z_df), unit = "log2")
+  SZ = entropy.empirical(rowSums(z_df), unit = "log2")
+
+  # homogeneity = 1 - sum((colSums(z_df)/sum(colSums(z_df)) * SjZ) / SZ)
+  # completeness = 1 - sum((rowSums(z_df)/sum(rowSums(z_df)) * SjR) / SR)
+
+  #map1
+  SjZ/SZ
+  # map2
+  SjR/SR
+
+  # B = 1
+  # vmeasure = ((1 + B) * homogeneity * completeness) / (B * homogeneity + completeness)
+
+  v_result = v_measure(x = colSums(z_df), y = rowSums(z_df), z = z_df, unit = unit, B = B)
   sabre_result = list(x, y, v_result)
   return(sabre_result)
 }
