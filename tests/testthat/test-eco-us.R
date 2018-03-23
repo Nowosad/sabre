a416ca0fc5fa08b7b6fbd263de43e5f10ade37e9
Nowosad <tupiszakaczki@gmail.com>
2018-03-23 14:44:28 -0400

    new vmeasure test

--- tests/testthat/test-eco-us.R
+++ tests/testthat/test-eco-us.R
@@ -1,21 +1,19 @@
 context("test-eco-us.R")
 data("eco_us")
-vm = sabre_calc(eco_us, PROVINCE, eco_us, SECTION)
+vm  = sabre_calc(eco_us, PROVINCE, eco_us, SECTION)
+vm2 = sabre_calc(eco_us, DOMAIN, eco_us, DIVISION)
+# vm2 = sabre_calc(eco_us, DOMAIN, eco_us, PROVINCE)
+# vm3 = sabre_calc(eco_us, DOMAIN, eco_us, SECTION)
+# vm4 = sabre_calc(eco_us, DIVISION, eco_us, PROVINCE)
+# vm5 = sabre_calc(eco_us, DIVISION, eco_us, SECTION)
+# vm6 = sabre_calc(eco_us, PROVINCE, eco_us, SECTION)
 mc = mapcurves_calc(eco_us, PROVINCE, eco_us, SECTION)
 
 test_that("sabre works on vector", {
   expect_equal(unlist(vm[[3]]), c(vmeasure = 0.797, homogeneity = 1, completeness = 0.662), tolerance = 0.001)
+  expect_true(vm[[3]]$vmeasure > vm2[[3]]$vmeasure)
 })
 
 test_that("mapcurves works on vector", {
   expect_equal(mc[[3]]$gof, 1, tolerance = 0.001)
 })
-
-
-
-# vm1 = sabre_calc(eco_us, DOMAIN, eco_us, DIVISION)[[3]]
-# vm2 = sabre_calc(eco_us, DOMAIN, eco_us, PROVINCE)[[3]]
-# vm3 = sabre_calc(eco_us, DOMAIN, eco_us, SECTION)[[3]]
-# vm4 = sabre_calc(eco_us, DIVISION, eco_us, PROVINCE)[[3]]
-# vm5 = sabre_calc(eco_us, DIVISION, eco_us, SECTION)[[3]]
-# vm6 = sabre_calc(eco_us, PROVINCE, eco_us, SECTION)[[3]]
