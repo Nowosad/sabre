2b69a968d2167a1388e16b28d9b588e2a400764a
jn <tupiszakaczki@gmail.com>
2018-03-22 20:22:26 -0400

    data docs

--- R/data.R
+++ R/data.R
@@ -4,3 +4,17 @@
 #'
 #' @source https://www.sciencebase.gov/catalog/item/54244abde4b037b608f9e23d
 "eco_us"
+
+#' Red regionalization
+#'
+#' Data of the red regionalization used in Figure 1
+#'
+#' @source ...
+"regions1"
+
+#' Blue regionalization
+#'
+#' Data of the blue regionalization used in Figure 1
+#'
+#' @source ...
+"regions2"
