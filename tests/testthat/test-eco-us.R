c837fda4f5d9cf67e736468087f911d1c3df95c6
Nowosad <tupiszakaczki@gmail.com>
2018-03-27 15:13:56 -0400

    tests updated to the tibble form

--- tests/testthat/test-eco-us.R
+++ tests/testthat/test-eco-us.R
@@ -1,7 +1,7 @@
 context("test-eco-us.R")
 data("eco_us")
 vm  = sabre_calc(eco_us, PROVINCE, eco_us, SECTION)
-vm2 = sabre_calc(eco_us, DOMAIN, eco_us, DIVISION)
+vm2 = sabre_calc(eco_us, DOMAIN, eco_us, DIVISION, precision = 1)
 # vm2 = sabre_calc(eco_us, DOMAIN, eco_us, PROVINCE)
 # vm3 = sabre_calc(eco_us, DOMAIN, eco_us, SECTION)
 # vm4 = sabre_calc(eco_us, DIVISION, eco_us, PROVINCE)
@@ -10,8 +10,8 @@ vm2 = sabre_calc(eco_us, DOMAIN, eco_us, DIVISION)
 mc = mapcurves_calc(eco_us, PROVINCE, eco_us, SECTION)
 
 test_that("sabre works on vector", {
-  expect_equal(unlist(vm[[3]]), c(vmeasure = 0.797, homogeneity = 1, completeness = 0.662), tolerance = 0.001)
-  expect_true(vm[[3]]$vmeasure > vm2[[3]]$vmeasure)
+  expect_equal(c(vm[[3]], vm[[4]], vm[[5]]), c(0.797, 1, 0.662), tolerance = 0.001)
+  expect_true(vm[[3]] > vm2[[3]])
 })
 
 test_that("mapcurves works on vector", {
@@ -19,5 +19,5 @@ test_that("mapcurves works on vector", {
 })
 
 test_that("mapcurves is homogeneity", {
-  expect_equal(mc[[3]]$gof, vm[[3]][[2]], tolerance = 0.001)
+  expect_equal(mc[[3]]$gof, vm[[4]], tolerance = 0.001)
 })
--- tests/testthat/test-fig1.R
+++ tests/testthat/test-fig1.R
@@ -6,9 +6,9 @@ vm = sabre_calc(regions1, z, regions2, z)
 mc = mapcurves_calc(regions1, z, regions2, z)
 
 test_that("sabre works on vector", {
-  expect_equal(unlist(vm[[3]]), c(vmeasure = 0.36, homogeneity = 0.32, completeness = 0.42), tolerance = 0.01)
-  expect_equal(vm[[1]]$sabre, c(0.86, 0.48, 0.48, 0.48), tolerance = 0.01)
-  expect_equal(vm[[2]]$sabre, c(0.39, 0.59, 0.86), tolerance = 0.01)
+  expect_equal(c(vm[[3]], vm[[4]], vm[[5]]), c(0.36, 0.32, 0.42), tolerance = 0.01)
+  expect_equal(vm[[1]][[1]]$sabre, c(0.86, 0.48, 0.48, 0.48), tolerance = 0.01)
+  expect_equal(vm[[2]][[1]]$sabre, c(0.39, 0.59, 0.86), tolerance = 0.01)
 })
 
 test_that("mapcurves works on vector", {
