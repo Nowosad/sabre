6effc86a3ed3637004a895f6fe3fa47abdf55735
jn <tupiszakaczki@gmail.com>
2018-11-13 13:28:55 +0100

    adds new data documentation

--- R/data.R
+++ R/data.R
@@ -9,16 +9,38 @@
 #'
 #' Data of the red regionalization used in Figure 1 of Stepinski and Nowosad (2018)
 #'
-#' @references Stepinski, Tomasz, and Jakub Nowosad. "Assessing a degree of
-#' spatial association between regionalizations or categorical maps using the
-#' information-theoretical V-measure."
+#' @references Nowosad, Jakub, and Tomasz F. Stepinski.
+#' "Spatial association between regionalizations using the information-theoretical V-measure."
+#' International Journal of Geographical Information Science (2018).
+#' https://doi.org/10.1080/13658816.2018.1511794
 "regions1"
 
 #' Blue regionalization
 #'
 #' Data of the blue regionalization used in Figure 1 of Stepinski and Nowosad (2018)
 #'
-#' @references Stepinski, Tomasz, and Jakub Nowosad. "Assessing a degree of
-#' spatial association between regionalizations or categorical maps using the
-#' information-theoretical V-measure."
+#' @references Nowosad, Jakub, and Tomasz F. Stepinski.
+#' "Spatial association between regionalizations using the information-theoretical V-measure."
+#' International Journal of Geographical Information Science (2018).
+#' https://doi.org/10.1080/13658816.2018.1511794
 "regions2"
+
+#' Red regionalization (raster version)
+#'
+#' Raster data of the red regionalization used in Figure 1 of Stepinski and Nowosad (2018)
+#'
+#' @references Nowosad, Jakub, and Tomasz F. Stepinski.
+#' "Spatial association between regionalizations using the information-theoretical V-measure."
+#' International Journal of Geographical Information Science (2018).
+#' https://doi.org/10.1080/13658816.2018.1511794
+"partitions1"
+
+#' Blue regionalization (raster version)
+#'
+#' Raster data of the blue regionalization used in Figure 1 of Stepinski and Nowosad (2018)
+#'
+#' @references Nowosad, Jakub, and Tomasz F. Stepinski.
+#' "Spatial association between regionalizations using the information-theoretical V-measure."
+#' International Journal of Geographical Information Science (2018).
+#' https://doi.org/10.1080/13658816.2018.1511794
+"partitions2"
