2d158cac39e627c7bfe7ab3171a6a2a9bf1c5958
jn <tupiszakaczki@gmail.com>
2018-11-13 13:29:43 +0100

    adds mapcurves tests for raster

--- tests/testthat/test-raster.R
+++ tests/testthat/test-raster.R
@@ -1,9 +1,9 @@
 context("test-eco-us.R")
-data("partition1")
-data("partition2")
+data("partitions1")
+data("partitions2")
 
-vm = vmeasure_calc(partition1, partition2)
-# mc = mapcurves_calc(partition1, partition2)
+vm = vmeasure_calc(partitions1, partitions2)
+mc = mapcurves_calc(partitions1, partitions2)
 
 test_that("sabre works on raster", {
   expect_equal(c(vm[[3]], vm[[4]], vm[[5]]), c(0.36, 0.32, 0.42), tolerance = 0.01)
@@ -11,7 +11,7 @@ test_that("sabre works on raster", {
   expect_equal(zonal(vm[[2]]$rih, vm[[2]]$map2)[, 2], c(0.39, 0.59, 0.86), tolerance = 0.01)
 })
 
-# test_that("mapcurves works on raster", {
-#   expect_equal(mc$gof, 0.61, tolerance = 0.01)
-# })
+test_that("mapcurves works on raster", {
+  expect_equal(mc$gof, 0.61, tolerance = 0.01)
+})
 
