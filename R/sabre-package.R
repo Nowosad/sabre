45d70b882dc7e757b12e4445162647dbd225bf19
Jakub Nowosad <tupiszakaczki@gmail.com>
2022-08-17 10:30:34 +0200

    updates roxygen

--- R/sabre-package.R
+++ R/sabre-package.R
@@ -1,5 +1,8 @@
 #' @keywords internal
 "_PACKAGE"
 
+## usethis namespace: start
 ## quiets concerns of R CMD check re: var names
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("area", "map1", "map2"))
+## usethis namespace: end
+NULL
